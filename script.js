d3a843f5c3974ef7705759488c3b9c17a5cc72ad
ryuka <domodachiiwa@gmail.com>
2026-04-18 15:02:22 -0300

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 const btn = document.getElementById("okBtn");
-const message = document.getElementById("message");
-
-const sounds = [
-  document.getElementById("sound1"),
-  document.getElementById("sound2")
-];
-
-const texts = [
-  "I love you.",
-  "사랑해요"
-];
-
-let index = 0;
+const sound = document.getElementById("sound");
 
 btn.addEventListener("click", () => {
-  // ⛔ HARD STOP all sounds
-  sounds.forEach(sound => {
-    sound.pause();
-    sound.currentTime = 0;
-  });
-
-  const currentSound = sounds[index];
-
-  // small delay to ensure stop worked
-  setTimeout(() => {
-    currentSound.play().catch(() => {});
-  }, 50);
-
-  // change text
-  message.textContent = texts[index];
-
-  // next
-  index = (index + 1) % sounds.length;
+  // restart sound cleanly
+  sound.pause();
+  sound.currentTime = 0;
+  sound.play();
 });
